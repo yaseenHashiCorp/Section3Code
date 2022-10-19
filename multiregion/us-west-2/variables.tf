360595ec45e981523c465b2f64d7ec9ba8c5c2f7
yaseenHashiCorp <yaseen@hashicorp.com>
2022-10-19 18:57:45 -0500

    Update variables.tf

--- multiregion/us-west-2/variables.tf
+++ multiregion/us-west-2/variables.tf
@@ -1,2 +1,2 @@
-variable "region" {}
+variable "region" {default                   = "us-west-2"}
 variable "prefix" {            description = "This is the environment where your webapp is deployed. qa, prod, or dev" }
